5d928219dc14ce195aa121c66bcc9a16aae6ee8f
JadiellyL <desenvolvedorajadielly@gmail.com>
2020-02-22 15:04:11 -0300

    Estruturas de repetição- parte I

---
+++ aula09/js/aula09.js
@@ -0,0 +1,19 @@
+function clicar(){
+	var cont = 1
+	var res = window.document.getElementById('foto')
+
+	while(cont <= 6){
+		res.innerHTML += `<p>Oi tudo bem? ${cont}</p>`
+		cont++
+	}
+}
+
+function clicar2(){
+	var cont = 1
+	var res = window.document.getElementById('foto2')
+	
+	do{
+		res.innerHTML += `<p>Sim, está tudo bem ${cont}</p>`
+		cont++
+	}while(cont <= 6)
+}
