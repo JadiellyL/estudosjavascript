1c2f718a208e2b5c6ee4dab9bf71207e7dd04ad7
JadiellyL <desenvolvedorajadielly@gmail.com>
2020-03-26 17:01:36 -0300

    Aula12- funções com parâmetros

--- aula12/js/eventos.js
+++ aula12/js/eventos.js
@@ -5,4 +5,37 @@ function soma(n1=0, n2=0){
 let msg = document.getElementById('msg')
 
 //let a = soma(2)
-msg.innerHTML = `A soma é: ${soma(2,6)}`
+msg.innerHTML += `<p>A soma é: ${soma(2,6)}</p>`
+
+function parimp(n){
+	if (n%2 == 0) {
+		return 'Par'
+	}else{
+		return 'Ímpar'
+	}
+}
+
+msg.innerHTML += `<p>O número é: ${parimp(7)}</p>`
+
+function fatorialsimples(x){
+	let fat = 1
+	for(let i = x; i > 1; i--){
+		fat *= i
+	}
+
+	return fat
+}
+
+let res = fatorialsimples(5)
+msg.innerHTML += `<p>O fatorialsimples é: ${res}</p>`
+
+function fatorial(n){
+	if (n == 1) {
+		return 1
+	}else{
+		return n * fatorial(n-1)
+	}
+}
+
+let res2 = fatorial(5)
+msg.innerHTML += `<p>O fatorial recursivo é: ${res2}</p>`
